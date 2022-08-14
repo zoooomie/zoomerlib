8b5c5187c52c14e1c7148b4340941045730aae03
zoooomie <haroldcox1988@protonmail.com>
2022-08-14 18:03:02 +0200

    Initial commit

---
+++ zoomio.h
@@ -0,0 +1,15 @@
+#ifndef ZOOMIO_H
+#define ZOOMIO_H
+
+#include <stdio.h>
+
+#define do_be_spittin_fr printf
+#define do_be_slurpin_fr scanf
+#define mf_spit putc
+#define mf_slurp getc
+#define yo_gib_sus_handle_fr fopen
+#define yo_yeet_sus_handle_fr fclose
+#define yo_git_dat_shiet_outa_hier_fr remove
+#define yo_sus_handle_do_be_flushin fflush
+
+#endif
---
+++ zoomzoom.h
@@ -0,0 +1,24 @@
+#ifndef ZOOMZOOM_H
+#define ZOOMZOOM_H
+
+#define rn ;
+#define cap 0
+#define no !
+#define chief int main(int argc, char** argv)
+#define do_be {
+#define frfr }
+#define fr (
+#define n ,
+#define mf )
+#define yikes break
+#define deadass continue
+#define yeet return
+#define bouta &
+#define finna =
+#define bussin :
+#define be ==
+#define sus switch
+#define squad struct
+#define gang union
+
+#endif

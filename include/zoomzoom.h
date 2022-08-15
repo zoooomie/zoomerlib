c6d0ada0ad601eb8d4401a189e155528470cc0ad
fuggy <45698918+nonetrix@users.noreply.github.com>
2022-08-15 08:05:53 -0500

    Make syntax more clear and add 💀

--- include/zoomzoom.h
+++ include/zoomzoom.h
@@ -1,30 +1,31 @@
 #ifndef ZOOMZOOM_H
 #define ZOOMZOOM_H
 
-#define rn ;
-#define cap 0
-#define no !
-#define chief int main(int argc, char** argv)
-#define do_be {
-#define frfr }
-#define fr (
-#define n ,
-#define mf )
-#define yikes break
-#define deadass continue
-#define yeet return
-#define bouta &
-#define finna =
-#define bussin :
-#define be ==
-#define aint !=
-#define gangup +
-#define sus switch
-#define squad struct
-#define gang union
-#define dead void
-#define cuh while
-#define trust const
-#define fo for
+#define 🕑rn🌞 ;
+#define cap🧢 0
+#define ✖️no❌ !
+#define 👩‍🍳chief👨‍🍳 int main(int argc, char** argv)
+#define 😐do_be😳💀🤨🤨🤨🤨🤨 {
+#define 😐frfr🤨🤨🤨🤨🤨🤨 }
+#define 😐fr🤨 (
+#define 😐n😐 ,
+#define 🫃mf🤱 )
+#define 😳yikes🤨🤨😐 break
+#define ☠️💀deadass🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨🤨 continue
+#define 🗑️yeet🚮 return
+#define 💢bouta😐🤨😳 &
+#define 💢finna😳 =
+#define bussy🍑🐱🍆💦💧 :
+#define 🤨be😳 ==
+#define ✖️aint❌ !=
+#define 👨‍👨‍👧‍👧gangup💪 +
+#define 💀 -
+#define ඞsusඞඞඞඞඞඞඞ😳 switch
+#define 👨‍👨‍👧‍👧squad💪 struct
+#define 👨‍👨‍👧‍👧gang💪 union
+#define ☠️dead💀 void
+#define 💪cuh🔁 while
+#define ❎✅✅✅✅✅✅✅✅trust✅ const
+#define 😐☠️💀😳fo😳 for
 
 #endif

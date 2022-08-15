6c962be1e7d9df3fe52bc0590ae6ad5e7448d8b3
ALIAS <69894931+colias-palaeno@users.noreply.github.com>
2022-08-15 08:07:58 +0100

    Create fizzbuzz.c
    I wrote this in pain.

---
+++ examples/fizzbuzz.c
@@ -0,0 +1,12 @@
+#include "../include/zoomzoom.h"
+#include "../include/zoomio.h"
+
+chief do_be
+	fo fr int i finna 1 rn i <= 100 rn i++ mf do_be
+		if fr i % 15 be cap mf do_be_spittin_fr fr "AYO this shit fizzin AND buzzin frfr\n" mf rn
+		else if fr i % 3 be cap mf do_be_spittin_fr fr "this shit fizzin frfr\n" mf rn
+		else if fr i % 5 be cap mf do_be_spittin_fr fr "this shit buzzin frfr\n" mf rn
+		else do_be_spittin_fr fr "%d\n", i mf rn
+	frfr
+	yeet cap rn
+frfr

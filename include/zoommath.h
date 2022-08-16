0ffb043a559bf6844b6c7d0216490ac0552b5dd5
Aphrim <gregperrett5@gmail.com>
2022-08-16 10:11:44 -0400

    WIP for zoommath.h

---
+++ include/zoommath.h
@@ -0,0 +1,23 @@
+#ifndef ZOOMMATH_H
+#define ZOOMMATH_H
+
+#include <math.h>
+
+#define coSUS cos
+#define sussin sin
+//tan?
+
+#define amogus_coSUS acos
+#define amogus_sussin asin
+//atan & atan2?
+
+#define exponential_ratio exp
+#define LLLrithm log
+
+#define bozo_became_positive abs
+#define to_the_moon ceil
+#define throwin floor
+
+#define num_be_cappin NAN
+
+#endif
